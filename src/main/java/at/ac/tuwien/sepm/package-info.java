cc65e65e2e70d7ed5416f177896c97c51870187c
Zhang Haixiang <haixiangzhang@yahoo.de>
2016-05-01 22:25:57 +0200

    Maven Projekterstellung mit vordefiniertem archtype von bpse

---
+++ src/main/java/at/ac/tuwien/sepm/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package is the root-package.<br>
+ * It contains the Start class
+ * to start the User-Interface, 
+ * and all other classes in sub-packages.
+ * 
+ * @author The SE-Team
+ * @version 1.0
+ * 
+ * 
+ */
+package at.ac.tuwien.sepm;
+
---
+++ src/test/java/at/ac/tuwien/sepm/test/dao/AllDAOTests.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.test.dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses( { JdbcStudentTest.class })
+public class AllDAOTests {
+}
