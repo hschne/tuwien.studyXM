08ae525520f47cd8e1f213e9ddc950cbdc6b0c46
Zhang Haixiang <haixiangzhang@yahoo.de>
2016-05-01 21:44:03 +0200

    removed projekt

--- src/main/java/at/ac/tuwien/sepm/helper/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * This Package contains various helper classes like a class containing 
- * application-wide constants.
- * 
- * @author The SE-Team
- * @version 1.0
- */
-package at.ac.tuwien.sepm.helper;
