f6f855b0683ef16c0a7425048de09a7a6f89f1bc
Zhang Haixiang <haixiangzhang@yahoo.de>
2016-05-01 21:37:26 +0200

    pom removed

---
+++ src/main/java/at/ac/tuwien/sepm/helper/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This Package contains various helper classes like a class containing 
+ * application-wide constants.
+ * 
+ * @author The SE-Team
+ * @version 1.0
+ */
+package at.ac.tuwien.sepm.helper;
