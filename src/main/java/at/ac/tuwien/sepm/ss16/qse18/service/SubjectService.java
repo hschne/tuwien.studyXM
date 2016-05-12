f17f921b369ac222c6b2d04686215ce7d7a2b2a5
Hans <hansjrgschroedl@gmail.com>
2016-05-12 22:58:58 +0200

    Kommentare in SubjectService einführen [#5359]

--- src/main/java/at/ac/tuwien/sepm/ss16/qse18/service/SubjectService.java
+++ src/main/java/at/ac/tuwien/sepm/ss16/qse18/service/SubjectService.java
@@ -11,14 +11,43 @@ import java.util.List;
 * */
 public interface SubjectService {
 
+    /**
+     * Returns a subject specified by the ID
+     * @param id The id of the subject
+     * @return A subject
+     * @throws ServiceException
+     */
     Subject getSubject(int id) throws ServiceException;
 
+    /**
+     * Implementations return a list of subjects
+     * @return A list of subjects
+     * @throws ServiceException
+     */
     List<Subject> getSubjects()throws ServiceException;
 
+    /**
+     * Implementations serve to create a subject
+     * @param subject The subject to create
+     * @return The created subject
+     * @throws ServiceException
+     */
     Subject createSubject(Subject subject)throws ServiceException;
 
+    /**
+     * Implementations delete a subject
+     * @param subject The subject to delete
+     * @return False if the operation failed, true if it succeeded
+     * @throws ServiceException
+     */
     boolean deleteSubject(Subject subject)throws ServiceException;
 
+    /**
+     * Implementations update a subject
+     * @param subject The subject to update
+     * @return The updated subject
+     * @throws ServiceException
+     */
     Subject updateSubject(Subject subject)throws ServiceException;
 
 }
