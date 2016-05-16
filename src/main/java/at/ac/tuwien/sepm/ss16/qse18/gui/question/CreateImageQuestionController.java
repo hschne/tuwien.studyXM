3bbf8d3af9a4c2c5a71b373c2d9b29e71bfe1810
Hans <hansjrgschroedl@gmail.com>
2016-05-16 17:11:43 +0200

    Verwende korrekten Konstruktor fuer Question [#5572]

--- src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/question/CreateImageQuestionController.java
+++ src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/question/CreateImageQuestionController.java
@@ -4,14 +4,10 @@ import at.ac.tuwien.sepm.ss16.qse18.domain.Answer;
 import at.ac.tuwien.sepm.ss16.qse18.domain.Question;
 import at.ac.tuwien.sepm.ss16.qse18.domain.QuestionType;
 import at.ac.tuwien.sepm.ss16.qse18.gui.GuiController;
-import at.ac.tuwien.sepm.ss16.qse18.gui.observableEntity.ObservableAnswer;
-import at.ac.tuwien.sepm.ss16.qse18.gui.observableEntity.ObservableQuestion;
 import at.ac.tuwien.sepm.ss16.qse18.service.QuestionService;
 import at.ac.tuwien.sepm.ss16.qse18.service.ServiceException;
 import at.ac.tuwien.sepm.util.AlertBuilder;
-import at.ac.tuwien.sepm.util.SpringFXMLLoader;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -37,8 +33,8 @@ import java.util.List;
  *
  * @author Julian on 14.05.2016.
  */
-@Component @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) public class CreateImageQuestionController
-        implements GuiController{
+@Component @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class CreateImageQuestionController implements GuiController {
 
     @FXML public Button buttonCreateQuestion;
     @FXML public Button buttonAddImage;
@@ -59,61 +55,49 @@ import java.util.List;
 
     private Logger logger = LoggerFactory.getLogger(CreateImageQuestionController.class);
     private AlertBuilder alertBuilder;
-    private SpringFXMLLoader springFXMLLoader;
-    private Stage primaryStage;
 
     private QuestionService questionService;
 
-    @Autowired
-    public CreateImageQuestionController(SpringFXMLLoader springFXMLLoader,
-                                         QuestionService questionService, AlertBuilder alertBuilder) {
-        this.springFXMLLoader = springFXMLLoader;
+    @Autowired public CreateImageQuestionController(QuestionService questionService,
+        AlertBuilder alertBuilder) {
         this.questionService = questionService;
         this.alertBuilder = alertBuilder;
     }
 
-    @FXML
-    public void handleCreateQuestion() throws IOException {
+    @FXML public void handleCreateQuestion() throws IOException {
         logger.debug("Creating new Question");
         try {
             Question q = newQuestionFromFields();
             questionService.createQuestion(q);
-            questionService.setCorrespondingAnswers(q,newAnswersFromFields());
+            questionService.setCorrespondingAnswers(q, newAnswersFromFields());
         } catch (ServiceException e) {
             showAlert(e);
         }
     }
 
     private Question newQuestionFromFields() {
-        Question newQuestion = new Question();
-        newQuestion.setQuestion(textFieldImagePath.getText());
-        newQuestion.setType(QuestionType.NOTECARD);
-        System.out.println(newQuestion.toString());
-        return newQuestion;
+        String imagePath = textFieldImagePath.getText();
+        return new Question(imagePath, QuestionType.NOTECARD, 0);
     }
 
-    private List<Answer> newAnswersFromFields(){
+    private List<Answer> newAnswersFromFields() {
         List<Answer> answers = new LinkedList<>();
 
-        if(!textFieldAnswerOne.getText().isEmpty())
-        {
-            answers.add(new Answer(
-                    QuestionType.NOTECARD,textFieldAnswerOne.getText(),checkBoxAnswerOne.isSelected()));
+        if (!textFieldAnswerOne.getText().isEmpty()) {
+            answers.add(new Answer(QuestionType.NOTECARD, textFieldAnswerOne.getText(),
+                checkBoxAnswerOne.isSelected()));
         }
-        if(!textFieldAnswerTwo.getText().isEmpty())
-        {
-            answers.add(new Answer(
-                    QuestionType.NOTECARD,textFieldAnswerTwo.getText(),checkBoxAnswerTwo.isSelected()));
+        if (!textFieldAnswerTwo.getText().isEmpty()) {
+            answers.add(new Answer(QuestionType.NOTECARD, textFieldAnswerTwo.getText(),
+                checkBoxAnswerTwo.isSelected()));
         }
-        if(!textFieldAnswerThree.getText().isEmpty())
-        {
-            answers.add(new Answer(
-                    QuestionType.NOTECARD,textFieldAnswerThree.getText(),checkBoxAnswerThree.isSelected()));
+        if (!textFieldAnswerThree.getText().isEmpty()) {
+            answers.add(new Answer(QuestionType.NOTECARD, textFieldAnswerThree.getText(),
+                checkBoxAnswerThree.isSelected()));
         }
-        if(!textFieldAnswerFour.getText().isEmpty())
-        {
-            answers.add(new Answer(
-                    QuestionType.NOTECARD,textFieldAnswerFour.getText(),checkBoxAnswerFour.isSelected()));
+        if (!textFieldAnswerFour.getText().isEmpty()) {
+            answers.add(new Answer(QuestionType.NOTECARD, textFieldAnswerFour.getText(),
+                checkBoxAnswerFour.isSelected()));
         }
         return answers;
     }
@@ -126,9 +110,10 @@ import java.util.List;
 
     /**
      * loads a new image into the imageViewQuestionImage and displays the path in the textFieldImagePath.
+     *
      * @throws IOException
      */
-    public void selectImage() throws IOException{
+    public void selectImage() throws IOException {
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Add image");
@@ -143,16 +128,12 @@ import java.util.List;
     }
 
     public void setPrimaryStage(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+        //Not yet needed!
     }
 
     private void showAlert(ServiceException e) {
-        Alert alert = alertBuilder
-                .alertType(Alert.AlertType.ERROR)
-                .title("Error")
-                .headerText("An error occured")
-                .contentText(e.getMessage())
-                .build();
+        Alert alert = alertBuilder.alertType(Alert.AlertType.ERROR).title("Error")
+            .headerText("An error occured").contentText(e.getMessage()).build();
         alert.showAndWait();
     }
 }
