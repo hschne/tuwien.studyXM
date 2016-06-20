a3306918c51455b3b96156cb8d9a484c93e29931
Philipp Ganiu <pganiu@gmail.com>
2016-06-20 13:48:13 +0200

    Grundgerüst für zweiter Versuch.[issue #5577]

--- src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/question/CreateSelfEvalQuestionController.java
+++ src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/question/CreateSelfEvalQuestionController.java
@@ -1,8 +1,19 @@
 package at.ac.tuwien.sepm.ss16.qse18.gui.question;
 
+import at.ac.tuwien.sepm.ss16.qse18.service.QuestionService;
+import at.ac.tuwien.sepm.ss16.qse18.service.ResourceQuestionService;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author Philipp Ganiu
  */
 public class CreateSelfEvalQuestionController extends CreateImageQuestionController {
-    
+    @FXML public TextField textFieldImagePath;
+
+    @Autowired public CreateSelfEvalQuestionController(QuestionService questionService,
+        ResourceQuestionService resourceQuestionService) {
+        super(questionService, resourceQuestionService);
+    }
 }
