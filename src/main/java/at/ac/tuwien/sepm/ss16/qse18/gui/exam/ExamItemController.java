3d84f514f93258b01f0fe2565be69bcfcda9d27c
Julian Strohmayer <julian.strohmayer@hotmail.com>
2016-06-21 12:00:22 +0200

    Prognose Anzeigen implementiert, auf dem Homescreen wird jetzt der Status eines Exams angezeigt. [#5894]

--- src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/exam/ExamItemController.java
+++ src/main/java/at/ac/tuwien/sepm/ss16/qse18/gui/exam/ExamItemController.java
@@ -10,32 +10,53 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Felix on 01.06.2016.
  */
-@Component @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) public class ExamItemController extends BaseController {
-    @FXML public Node root;
-    @Autowired ExerciseExamService exerciseExamService;
-    @Autowired ExamService examService;
-    @FXML private Label examIdentifier;
-    @FXML private ProgressBar examProgress;
-    @FXML private Button buttonStudy;
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class ExamItemController extends BaseController {
+    @FXML
+    public Node root;
+    @Autowired
+    ExerciseExamService exerciseExamService;
+    @Autowired
+    ExamService examService;
+    @FXML
+    private Label examIdentifier;
+    @FXML
+    private ProgressBar examProgress;
+    @FXML
+    private Button buttonStudy;
+    @FXML
+    private Label labelProgress;
+    @FXML
+    private ImageView imageViewProgress;
     private ObservableExam exam;
 
-    @Autowired ExamItemController(ExerciseExamService exerciseExamService, ExamService examService) {
+    @Autowired
+    ExamItemController(ExerciseExamService exerciseExamService, ExamService examService) {
         this.exerciseExamService = exerciseExamService;
         this.examService = examService;
     }
 
-    @FXML public void handleStudy() {
+    @FXML
+    public void handleStudy() {
         mainFrameController.handleStudy(exam);
     }
 
@@ -53,7 +74,7 @@ import java.util.List;
         try {
             exerciseExamList = examService.getAllExerciseExamsOfExam(exam.getExamInstance());
             logger.debug("Got list of exercise exams of size " + exerciseExamList.size());
-            for(Integer i : exerciseExamList) {
+            for (Integer i : exerciseExamList) {
                 allQuestionList.addAll(exerciseExamService.getAllQuestionsOfExam(i));
                 answeredQuestions.addAll(exerciseExamService.getAnsweredQuestionsOfExam(i));
             }
@@ -66,21 +87,56 @@ import java.util.List;
 
         if (!allQuestionList.isEmpty()) {
             percentageAnswered =
-                (double) answeredQuestions.size() / (double) allQuestionList.size();
+                    (double) answeredQuestions.size() / (double) allQuestionList.size();
             examProgress.setProgress(percentageAnswered);
         }
 
         logger.debug("Already answered: " + answeredQuestions.size() + "/" +
-            allQuestionList.size() + " " + percentageAnswered);
+                allQuestionList.size() + " " + percentageAnswered);
 
         if (percentageAnswered > 0.8d) {
             examProgress.setStyle("-fx-accent:#A2E88B;");
+
         } else if (percentageAnswered > 0.3d) {
             examProgress.setStyle("-fx-accent:#F0F595;");
         } else {
             examProgress.setStyle("-fx-accent:#F7A099;");
         }
+
+        checkProgress(percentageAnswered);
     }
+
+    /**
+     * This method calculates the time difference in days between the current date and the
+     * exam due date. When the progress is critical a hint will be displayed in the GUI.
+     * @param progress amount of answered questions
+     */
+    private void checkProgress(double progress){
+        logger.debug("calculating time difference ");
+        SimpleDateFormat format = new SimpleDateFormat("dd-mm-yyyy");
+        Date d1 = null;
+        Date d2 = null;
+        try {
+            d1 = format.parse(format.format(new Date()));
+            d2 = format.parse(exam.getDueDate());
+
+            long difference = d2.getTime() - d1.getTime();
+            long differenceHours = difference / (60 * 60 * 1000) % 24;
+            long differenceDays = difference / (24 * 60 * 60 * 1000);
+
+            if (progress < 0.5d && differenceDays < 7L && differenceDays >= 0) {
+                imageViewProgress.setImage(new Image("/icons/badProgress.png"));
+                labelProgress.setText("your progress is critical, " +
+                        differenceDays + (differenceDays != 1 ? " days " : " day ") +
+                        differenceHours + (differenceHours!=1?" hours ":"hour ")+
+                        "till the exam.");
+            }
+
+        } catch (ParseException e) {
+            logger.error("Unable to calculate time difference", e);
+        }
+    }
+
 
     public Node getRoot() {
         return this.root;
